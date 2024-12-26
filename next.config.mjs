c558db70fe2eb5d73ffa3375dfd98e0dd5302f56
Dheeraj Agrahari <dheeraj.a@napses.com>
2024-12-26 22:19:58 +0530

    fixed some chnages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com','res.cloudinary.com','images.unsplash.com','media.istockphoto.com'], 
   },
