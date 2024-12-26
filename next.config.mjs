7446b313e4992d905b6ba0d1cc9b8e0eb109e893
Dheeraj Agrahari <dheeraj.a@napses.com>
2024-12-26 22:24:18 +0530

    remove eslist

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['lh3.googleusercontent.com','res.cloudinary.com','images.unsplash.com','media.istockphoto.com'], 
   },
